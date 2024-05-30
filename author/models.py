8ecc68f4c284402f11a4322f80febad4b36540f6
Alla Kuksa <alla.kuksa92@gmail.com>
2024-05-30 17:18:22 +0300

    Solution

--- author/models.py
+++ author/models.py
@@ -4,6 +4,14 @@ from django.db import models
 class Author(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
-    pseudonym = models.CharField(max_length=64, null=True, blank=True)
+    pseudonym = models.CharField(
+        max_length=64,
+        null=True,
+        blank=True
+    )
     age = models.IntegerField()
     retired = models.BooleanField()
+
+    def __str__(self):
+        return (f"Author {self.first_name} {self.last_name}, "
+                f"pseudonym {self.pseudonym}, age {self.age}")
